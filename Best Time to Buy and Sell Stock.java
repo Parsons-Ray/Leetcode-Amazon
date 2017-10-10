3c33a8f4f5b37e48f66517db270d27076c056332
Parsons-Ray <32499337+Parsons-Ray@users.noreply.github.com>
2017-10-10 15:35:26 -0700

    Add files via upload

---
+++ Best Time to Buy and Sell Stock.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE, res = 0;
+        for(int i = 0 ; i < prices.length; i++){
+            if(prices[i]<min) min = prices[i];
+            if((prices[i]-min)>res) res = prices[i] - min;
+        }
+        return res;
+    }
+}
+
+
